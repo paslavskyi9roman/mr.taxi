a76bdc3078ea56e8bd81d04737181b495640a03d
Roman Paslavskyi <roman.paslavskyi@elements.cloud>
2024-11-09 15:28:06 +0200

    ngx-translate installed and configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {TranslateModule, TranslatePipe, TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslatePipe, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'mr-taxi';
+export class AppComponent implements OnInit {
+  private translate: TranslateService = inject(TranslateService);
+
+  ngOnInit(): void {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
