8dcd8c608fa0dcd42a75c448f9368ffcf893f316
paslavskyi9roman <paslavsky.roman@gmail.com>
2024-11-16 20:30:33 +0200

    created modal component

---
+++ src/app/shared/components/mt-modal/mt-modal.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mt-modal',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './mt-modal.component.html',
+  styleUrl: './mt-modal.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MtModalComponent {
+  @Input() title: string = '';
+  @Output() close = new EventEmitter<void>();
+}
