3be5a87194124247911fdf71318d6f44a4fde28b
Roman Paslavskyi <roman.paslavskyi@elements.cloud>
2024-11-16 13:47:43 +0200

    work on a UI layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import { TranslateModule, TranslatePipe, TranslateService } from '@ngx-translate/core';
-import { Theme } from './core/models/theme';
+
 import { Language } from './core/models/language';
-import {ThemeService} from './core/services/theme.service';
+import { LayoutComponent } from './shared/components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslatePipe, TranslateModule],
+  imports: [TranslatePipe, TranslateModule, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   private translate: TranslateService = inject(TranslateService);
-  protected readonly themeService: ThemeService = inject(ThemeService);
-  protected readonly Language = Language;
-
-  public get themeLabel(): string {
-    return this.themeService.theme === Theme.Light ? 'Dark' : 'Light';
-  }
 
   public ngOnInit(): void {
     this.translate.setDefaultLang(Language.English);
     this.translate.use(Language.English);
-  }
-
-  public switchLanguage(lang: Language): void {
-    this.translate.use(lang);
   }
 }
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { TranslateModule, TranslatePipe, TranslateService } from '@ngx-translate/core';
+
+import { ThemeService } from '../../core/services/theme.service';
+import { Theme } from '../../core/models/theme';
+import { Language } from '../../core/models/language';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  private translate: TranslateService = inject(TranslateService);
+  protected readonly themeService: ThemeService = inject(ThemeService);
+  protected readonly Language = Language;
 
+  public get themeLabel(): string {
+    return this.themeService.theme === Theme.Light ? 'Dark' : 'Light';
+  }
+
+  public switchLanguage(lang: Language): void {
+    this.translate.use(lang);
+  }
 }
