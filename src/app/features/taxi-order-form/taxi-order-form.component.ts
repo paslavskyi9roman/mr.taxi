44cc287cc0e02457a92f9dc7c63fc0a3db26a32c
Roman Paslavskyi <roman.paslavskyi@elements.cloud>
2024-11-23 16:03:35 +0200

    taxi form order implemented

--- src/app/features/taxi-order-form/taxi-order-form.component.ts
+++ src/app/features/taxi-order-form/taxi-order-form.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, FormArray } from '@angular/forms';
 import { MtButtonComponent } from '../../shared/components/mt-button/mt-button.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-taxi-order-form',
   standalone: true,
-  imports: [ReactiveFormsModule, MtButtonComponent],
+  imports: [ReactiveFormsModule, MtButtonComponent, TranslatePipe],
   templateUrl: './taxi-order-form.component.html',
   styleUrls: ['./taxi-order-form.component.scss']
 })
 export class TaxiOrderFormComponent {
   public taxiOrderForm: FormGroup;
   public showAdditionalStops: boolean = false;
+  public additionalStops: FormArray;
 
   private readonly formBuilder: FormBuilder;
 
@@ -19,13 +21,14 @@ export class TaxiOrderFormComponent {
     this.formBuilder = formBuilder;
     this.taxiOrderForm = this.formBuilder.group({
       from: ['', Validators.required],
-      additionalStops: [''],
+      additionalStops: this.formBuilder.array([]),
       to: ['', Validators.required],
       passengerName: ['', Validators.required],
       numberOfPassengers: [1, Validators.required],
-      passengerPhoneNumber: ['', Validators.required],
+      passengerPhoneNumber: ['', [Validators.required, Validators.pattern(/^\+?[1-9]\d{1,14}$/)]],
       comment: ['']
     });
+    this.additionalStops = this.taxiOrderForm.get('additionalStops') as FormArray;
   }
 
   public onSubmit(): void {
@@ -44,5 +47,13 @@ export class TaxiOrderFormComponent {
     if (currentValue > 1) {
       this.taxiOrderForm.get('numberOfPassengers')?.setValue(currentValue - 1);
     }
+  }
+
+  public addStop(): void {
+    this.additionalStops.push(this.formBuilder.control(''));
+  }
+
+  public removeStop(index: number): void {
+    this.additionalStops.removeAt(index);
   }
 }
