874872b2b1b0d068ae46aed0339e58a1d905dfdc
paslavskyi9roman <paslavsky.roman@gmail.com>
2024-11-24 17:29:12 +0200

    updated time input

--- src/app/features/additional-info-form/additional-info-form.component.ts
+++ src/app/features/additional-info-form/additional-info-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MtButtonComponent } from '../../shared/components/mt-button/mt-button.component';
 import { CommonModule } from '@angular/common';
@@ -9,15 +9,17 @@ import { MtSlideToggleComponent } from '../../shared/components/mt-slide-toggle/
   standalone: true,
   imports: [ReactiveFormsModule, MtButtonComponent, CommonModule, MtSlideToggleComponent],
   templateUrl: './additional-info-form.component.html',
-  styleUrls: ['./additional-info-form.component.scss']
+  styleUrls: ['./additional-info-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AdditionalInfoFormComponent {
+export class AdditionalInfoFormComponent implements OnInit {
   public additionalInfoForm: FormGroup;
   @Output() public formSubmitted = new EventEmitter<any>();
 
   public showLuggage = false;
   public showScheduledRide = false;
   public showFlightNumber = false;
+  public currentTime = '';
 
   constructor(private formBuilder: FormBuilder) {
     this.additionalInfoForm = this.formBuilder.group({
@@ -25,6 +27,9 @@ export class AdditionalInfoFormComponent {
       scheduledRide: [''],
       flightNumber: ['']
     });
+  }
+  ngOnInit(): void {
+    this.getCurrentTime();
   }
 
   public toggleLuggage(checked: boolean): void {
@@ -52,5 +57,12 @@ export class AdditionalInfoFormComponent {
     if (this.additionalInfoForm.valid) {
       this.formSubmitted.emit(this.additionalInfoForm.value);
     }
+  }
+
+  public getCurrentTime(): void {
+    this.currentTime = new Date().toLocaleTimeString('en-GB', {
+      hour: '2-digit',
+      minute: '2-digit'
+    });
   }
 }
