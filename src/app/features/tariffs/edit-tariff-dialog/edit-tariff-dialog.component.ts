75c65582ccf6cd8cf3a31e87c37985aa1a864689
paslavskyi9roman <paslavsky.roman@gmail.com>
2025-03-07 15:41:01 +0200

    feat: enhance edit tariff dialog with dynamic additional stops functionality and improved layout

--- src/app/features/tariffs/edit-tariff-dialog/edit-tariff-dialog.component.ts
+++ src/app/features/tariffs/edit-tariff-dialog/edit-tariff-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MtButtonComponent } from '../../../shared/components/mt-button/mt-button.component';
 import { Tariff } from '../tariff.model';
 import { TranslatePipe } from '@ngx-translate/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-edit-tariff-dialog',
@@ -21,12 +22,14 @@ import { TranslatePipe } from '@ngx-translate/core';
     MatButtonModule,
     ReactiveFormsModule,
     MtButtonComponent,
-    TranslatePipe
+    TranslatePipe,
+    MatIcon
   ],
   styleUrls: ['./edit-tariff-dialog.component.scss']
 })
 export class EditTariffDialogComponent {
   public form: FormGroup;
+  public additionalStops: FormArray;
 
   constructor(
     private fb: FormBuilder,
@@ -36,8 +39,10 @@ export class EditTariffDialogComponent {
     this.form = this.fb.group({
       from: [data.route.from, Validators.required],
       to: [data.route.to, Validators.required],
-      price: [data.price, Validators.required]
+      price: [data.price, Validators.required],
+      additionalStops: this.fb.array(data.additionalStops.map((stop) => this.fb.control(stop.from)))
     });
+    this.additionalStops = this.form.get('additionalStops') as FormArray;
   }
 
   public save(): void {
@@ -46,10 +51,19 @@ export class EditTariffDialogComponent {
       const updatedTariff: Tariff = {
         ...this.data,
         price: formValue.price,
-        route: { from: formValue.from, to: formValue.to }
+        route: { from: formValue.from, to: formValue.to },
+        additionalStops: formValue.additionalStops.map((stop: string) => ({ from: stop, to: stop }))
       };
       this.dialogRef.close(updatedTariff);
     }
+  }
+
+  public addStop(): void {
+    this.additionalStops.push(this.fb.control(''));
+  }
+
+  public removeStop(index: number): void {
+    this.additionalStops.removeAt(index);
   }
 
   public close(): void {
